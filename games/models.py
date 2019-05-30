ac86a0021ca8c8b1894d6710821750b71823c720
jmur <jori.mur@gmail.com>
2019-05-30 22:06:56 +0200

    Add Game and create migrations

--- games/models.py
+++ games/models.py
@@ -1,3 +1,37 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Game(models.Model):
+   title = models.CharField(max_length=150)
+   author = models.CharField(max_length=70)
+   publisher = models.CharField(max_length=70)
+   numberofplayersminimum = models.IntegerField()
+   numberofplayersmaximum = models.IntegerField()
+   age = models.IntegerField()
+   duration = models.DurationField() 
+   type = models.CharField(max_length=200)
+   review = models.TextField(blank=True, null=True)
+   date_reviewed = models.DateTimeField(blank=True, null=True)
+  # score = 
+   CONCENTRATIE = 'CR'
+   GEDULD = 'GD'
+   GEHEUGEN = 'GH'
+   HOOFDREKENEN = 'HR'
+   DURF = 'DR'
+   HERKENNING = 'HK'
+   REACTIEVERMOGEN = 'RV'
+   SKILLS_CHOICES = (
+       (CONCENTRATIE, 'Concentratie'),
+       (GEDULD, 'Geduld'),
+       (GEHEUGEN, 'Geheugen'),
+       (HOOFDREKENEN, 'Hoofdrekenen'),
+       (DURF, 'Durf'),
+       (HERKENNING, 'Herkenning'),
+       (REACTIEVERMOGEN, 'Reactievermogen'),
+    )
+   skills = models.CharField(
+       max_length=2,
+       choices=SKILLS_CHOICES,
+    )
