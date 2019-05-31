681a5377f8ba72ca1dfd65f0edfa4f057d93b34f
jmur <jori.mur@gmail.com>
2019-05-31 08:39:06 +0200

    Added images and renamed the labels of model Game

--- games/models.py
+++ games/models.py
@@ -4,34 +4,38 @@ from django.utils import timezone
 
 
 class Game(models.Model):
-   title = models.CharField(max_length=150)
-   author = models.CharField(max_length=70)
-   publisher = models.CharField(max_length=70)
-   numberofplayersminimum = models.IntegerField()
-   numberofplayersmaximum = models.IntegerField()
-   age = models.IntegerField()
-   duration = models.DurationField() 
-   type = models.CharField(max_length=200)
-   review = models.TextField(blank=True, null=True)
-   date_reviewed = models.DateTimeField(blank=True, null=True)
+    image = models.ImageField(blank=True,null=True)
+    title = models.CharField(max_length=150, verbose_name="Titel")
+    author = models.CharField(max_length=70, verbose_name="Auteur")
+    publisher = models.CharField(max_length=70,verbose_name="Uitgever")
+    numberofplayersminimum = models.IntegerField(verbose_name="Minimum aantal spelers")
+    numberofplayersmaximum = models.IntegerField(verbose_name="Maximum aantal spelers")
+    age = models.IntegerField(verbose_name="Vanaf welke leeftijd")
+    duration = models.DurationField(verbose_name="Duur", help_text="Noteer het zo: [mm:ss]") 
+    type = models.CharField(max_length=200)
+    review = models.TextField(blank=True, null=True)
+    date_reviewed = models.DateTimeField(blank=True, null=True)
   # score = 
-   CONCENTRATIE = 'CR'
-   GEDULD = 'GD'
-   GEHEUGEN = 'GH'
-   HOOFDREKENEN = 'HR'
-   DURF = 'DR'
-   HERKENNING = 'HK'
-   REACTIEVERMOGEN = 'RV'
-   SKILLS_CHOICES = (
-       (CONCENTRATIE, 'Concentratie'),
-       (GEDULD, 'Geduld'),
-       (GEHEUGEN, 'Geheugen'),
-       (HOOFDREKENEN, 'Hoofdrekenen'),
-       (DURF, 'Durf'),
-       (HERKENNING, 'Herkenning'),
-       (REACTIEVERMOGEN, 'Reactievermogen'),
+    CONCENTRATIE = 'CR'
+    GEDULD = 'GD'
+    GEHEUGEN = 'GH'
+    HOOFDREKENEN = 'HR'
+    DURF = 'DR'
+    HERKENNING = 'HK'
+    REACTIEVERMOGEN = 'RV'
+    SKILLS_CHOICES = (
+        (CONCENTRATIE, 'Concentratie'),
+        (GEDULD, 'Geduld'),
+        (GEHEUGEN, 'Geheugen'),
+        (HOOFDREKENEN, 'Hoofdrekenen'),
+        (DURF, 'Durf'),
+        (HERKENNING, 'Herkenning'),
+        (REACTIEVERMOGEN, 'Reactievermogen'),
     )
-   skills = models.CharField(
-       max_length=2,
-       choices=SKILLS_CHOICES,
+    skill = models.CharField(
+        max_length=2,
+        choices=SKILLS_CHOICES,
+		verbose_name="Vaardigheid"
     )
+    def __str__(self):
+	    return self.title
