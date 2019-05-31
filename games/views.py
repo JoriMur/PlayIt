c2ba3d049e4906a3d64235254535ca5a379de912
jmur <jori.mur@gmail.com>
2019-05-31 22:41:06 +0200

    Created first URL and view

--- games/views.py
+++ games/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+# from django.http import HttpResponse
 # Create your views here.
+
+def games_list(request):
+    return render(request, 'games/games_list.html', {})
+
+
+#def games_list(request):
+#    return HttpResponse("We could put anything here")
+#    return HttpResponse(request.user.username)
